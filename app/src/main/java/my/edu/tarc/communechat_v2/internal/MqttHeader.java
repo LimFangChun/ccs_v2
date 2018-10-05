24e8224457f43fb9ede16717209fe41749d4360e
LimFangChun <leo477831@gmail.com>
2018-10-05 12:26:30 +0800

    Added mqtt header messages

---
+++ app/src/main/java/my/edu/tarc/communechat_v2/internal/MqttHeader.java
@@ -0,0 +1,30 @@
+package my.edu.tarc.communechat_v2.internal;
+
+public final class MqttHeader {
+    public static final String SUCCESS = "SUCCESS";
+    public static final String NO_RESULT = "NO_RESULT";
+    public static final String DUPLICATED = "DUPLICATED";
+    public static final String LOGIN = "LOGIN";
+    public static final String LOGIN_REPLY = "LOGIN_REPLY";
+    public static final String REGISTER_USER = "REGISTER_USER";
+    public static final String UPDATE_STUDENT = "UPDATE_STUDENT";
+    public static final String UPDATE_USER = "UPDATE_USER";
+    public static final String GET_FRIEND_LIST = "GET_FRIEND_LIST";
+    public static final String GET_FRIEND_LIST_REPLY = "GET_FRIEND_LIST_REPLY";
+    public static final String FIND_BY_ADDRESS = "FIND_BY_ADDRESS";
+    public static final String FIND_BY_ADDRESS_REPLY = "FIND_BY_ADDRESS_REPLY";
+    public static final String FIND_BY_PROGRAMME = "FIND_BY_PROGRAMME";
+    public static final String FIND_BY_PROGRAMME_REPLY = "FIND_BY_PROGRAMME_REPLY";
+    public static final String FIND_BY_TUTORIAL_GROUP = "FIND_BY_TUTORIAL_GROUP";
+    public static final String FIND_BY_TUTORIAL_GROUP_REPLY = "FIND_BY_TUTORIAL_GROUP_REPLY";
+    public static final String FIND_BY_AGE = "FIND_BY_AGE";
+    public static final String FIND_BY_AGE_REPLY = "FIND_BY_AGE_REPLY";
+    public static final String REQ_ADD_FRIEND = "REQ_ADD_FRIEND";
+    public static final String REQ_ADD_FRIEND_REPLY = "REQ_ADD_FRIEND_REPLY";
+    public static final String ADD_FRIEND = "ADD_FRIEND";
+    public static final String ADD_FRIEND_REPLY = "ADD_FRIEND_REPLY";
+    public static final String DELETE_FRIEND = "DELETE_FRIEND";
+    public static final String DELETE_FRIEND_REPLY = "DELETE_FRIEND_REPLY";
+    public static final String SEARCH_USERNAME = "SEARCH_USERNAME";
+    public static final String SERACH_USERNAME_REPLY = "SERACH_USERNAME_REPLY";
+}

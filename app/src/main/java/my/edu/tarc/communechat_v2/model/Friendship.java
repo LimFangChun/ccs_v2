9a532626c43bd3f0faaca327e6e0b7e91e7b4b68
LimFangChun <leo477831@gmail.com>
2018-10-05 12:52:01 +0800

    Added Friendship class to match with database

---
+++ app/src/main/java/my/edu/tarc/communechat_v2/model/Friendship.java
@@ -0,0 +1,53 @@
+package my.edu.tarc.communechat_v2.model;
+
+import java.util.Date;
+
+public class Friendship {
+    private int user_id;
+    private int friend_id;
+    private String status;
+    private Date date_created;
+
+    public Friendship(){
+
+    }
+
+    public Friendship(int user_id, int friend_id, String status, Date date_created) {
+        this.user_id = user_id;
+        this.friend_id = friend_id;
+        this.status = status;
+        this.date_created = date_created;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getFriend_id() {
+        return friend_id;
+    }
+
+    public void setFriend_id(int friend_id) {
+        this.friend_id = friend_id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Date date_created) {
+        this.date_created = date_created;
+    }
+}

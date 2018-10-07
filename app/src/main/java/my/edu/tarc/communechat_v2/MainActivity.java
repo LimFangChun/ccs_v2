fbb8e8310a3b405bd8b4b84993916f0775e5b40a
LimFangChun <leo477831@gmail.com>
2018-10-07 15:38:20 +0800

    No big deal here.

--- app/src/main/java/my/edu/tarc/communechat_v2/MainActivity.java
+++ app/src/main/java/my/edu/tarc/communechat_v2/MainActivity.java
@@ -7,7 +7,12 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import my.edu.tarc.communechat_v2.internal.MqttHelper;
+import my.edu.tarc.communechat_v2.model.User;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MqttHelper mqttHelper;
 
     private SharedPreferences pref;
     private TextView textViewHello;
@@ -16,14 +21,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mqttHelper = new MqttHelper();
+
         textViewHello = (TextView)findViewById(R.id.textViewHello);
         pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
-        if (pref == null || pref.getString("UID", null) == null){
+        if (pref == null ||  pref.getInt(User.COL_USER_ID, 0) == 0){
             Intent intentLogin = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intentLogin);
         }else{
-            textViewHello.setText("UID: "+ pref.getInt("UID", 0));
+            textViewHello.setText("user_id: "+ pref.getInt(User.COL_USER_ID, 0));
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mqttHelper.disconnect();
     }
 }

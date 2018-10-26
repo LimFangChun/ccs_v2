c9760cccae77ab7061ffcec5dce76dad65a65aac
JuzAPpl <32742178+JuzAPpl@users.noreply.github.com>
2018-10-26 13:06:41 +0800

    Revert text that changed accidentally

--- app/src/main/java/my/edu/tarc/communechat_v2/internal/GPSTracker.java
+++ app/src/main/java/my/edu/tarc/communechat_v2/internal/GPSTracker.java
@@ -149,7 +149,7 @@ public class GPSTracker extends Service implements LocationListener {
 
     /**
      * Function to show settings alert dialog
-     * On pressing SettingsActivity button will lauch SettingsActivity Options
+     * On pressing Settings button will lauch Settings Options
      */
     public void showSettingsAlert() {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
@@ -160,8 +160,8 @@ public class GPSTracker extends Service implements LocationListener {
         // Setting Dialog Message
         alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
 
-        // On pressing SettingsActivity button
-        alertDialog.setPositiveButton("SettingsActivity", new DialogInterface.OnClickListener() {
+        // On pressing Settings button
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 mContext.startActivity(intent);
